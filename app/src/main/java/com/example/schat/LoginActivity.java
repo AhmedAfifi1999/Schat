01533a86bf21978421eeefbd284b21d5de5d9f58
AhmedAfifi1999 <ahmedafifi1500@gmail.com>
2020-03-03 11:22:23 +0200

    update ids

--- app/src/main/java/com/example/schat/LoginActivity.java
+++ app/src/main/java/com/example/schat/LoginActivity.java
@@ -5,21 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private Toolbar mtoolbar;
+    private EditText UserEmail,UserPassword;
+    private Button Login_btn;
+    private TextView ForgetPasswordLink,AlreadyHaveAccountLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        InitialaizeFields();
 
         mtoolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mtoolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Login");
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void InitialaizeFields() {
+        Login_btn = findViewById(R.id.Login_btn);
+        UserEmail = findViewById(R.id.LEmail);
+        UserPassword = findViewById(R.id.Lpass);
+        AlreadyHaveAccountLink = findViewById(R.id.LAlready_have_Account);
+        ForgetPasswordLink = findViewById(R.id.LForget_password);
 
     }
 }
