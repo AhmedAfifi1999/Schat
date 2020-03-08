50458d0fc63b79009f0892d4a9c4bbf0f7dc6842
AhmedAfifi1999 <ahmedafifi1500@gmail.com>
2020-03-08 18:32:18 +0200

    Group Chat

--- app/src/main/java/com/example/schat/GroupsFragment.java
+++ app/src/main/java/com/example/schat/GroupsFragment.java
@@ -3,11 +3,25 @@ package com.example.schat;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 
 /**
@@ -15,6 +29,12 @@ import android.view.ViewGroup;
  */
 public class GroupsFragment extends Fragment {
 
+    private View groupFragmentView;
+    private ListView list_view;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_group = new ArrayList<>();
+
+    private DatabaseReference GroupRef;
 
     public GroupsFragment() {
         // Required empty public constructor
@@ -25,7 +45,43 @@ public class GroupsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groups, container, false);
+        groupFragmentView = inflater.inflate(R.layout.fragment_groups, container, false);
+        GroupRef = FirebaseDatabase.getInstance().getReference().child("Groups");
+
+        InitializeFields();
+
+        RetrieveAndDisplayGroups();
+        return groupFragmentView;
+    }
+
+    private void RetrieveAndDisplayGroups() {
+        GroupRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<>();
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+
+                while (iterator.hasNext()) {
+                    set.add(((DataSnapshot) iterator.next()).getKey());
+
+                }
+                list_of_group.clear();
+                list_of_group.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void InitializeFields() {
+        list_view = groupFragmentView.findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list_of_group);
+        list_view.setAdapter(arrayAdapter);
+
     }
 
 }
