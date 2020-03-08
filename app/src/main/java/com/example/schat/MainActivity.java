1d6050e98b9a5759ee769bc642840a111f117e3e
AhmedAfifi1999 <ahmedafifi1500@gmail.com>
2020-03-08 16:19:53 +0200

    add 1

--- app/src/main/java/com/example/schat/MainActivity.java
+++ app/src/main/java/com/example/schat/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.schat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -75,10 +81,10 @@ public class MainActivity extends AppCompatActivity {
         RootRef.child("Users").child(CurrentUserID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.child("name").exists()){
+                if (dataSnapshot.child("name").exists()) {
 
                     Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
 
 
                     SendUserToSettingsActivity();
@@ -93,15 +99,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void SendUserToLoginActivity() {
-        Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
-        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
         finish();
     }
+
     private void SendUserToSettingsActivity() {
 
-        Intent settingIntent = new Intent(MainActivity.this,SettingsActivity.class);
-        settingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        Intent settingIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        settingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingIntent);
         finish();
     }
@@ -131,7 +138,56 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+        if (item.getItemId() == R.id.main_create_group_option) {
 
+            RequestNewGroup();
+        }
         return true;
+    }
+
+    private void RequestNewGroup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialog);
+        builder.setTitle("Enter Group Name : ");
+
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("e.g G1 ");
+        builder.setView(groupNameField);
+        builder.setPositiveButton("Create ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String groupName = groupNameField.getText().toString();
+                if (TextUtils.isEmpty(groupName)) {
+
+                    Toast.makeText(MainActivity.this, "Pleas Write Group Name ", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    CreateNewGroup(groupName);
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+    }
+
+    private void CreateNewGroup(final String groupName) {
+        RootRef.child("Groups").child(groupName).setValue("").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+
+                    Toast.makeText(MainActivity.this, "group Name " + groupName + " Is Created successfully", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/schat/SettingsActivity.java
+++ app/src/main/java/com/example/schat/SettingsActivity.java
@@ -15,8 +15,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -41,11 +44,47 @@ public class SettingsActivity extends AppCompatActivity {
         RootRef = FirebaseDatabase.getInstance().getReference();
         CurrentUserID = mAuth.getCurrentUser().getUid();
         InitializeField();
+        userName.setVisibility(View.INVISIBLE);
 
         UpdateAccountSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 UpdateSetting();
+            }
+        });
+
+        RetrieveUserInfo();
+    }
+
+    private void RetrieveUserInfo() {
+        RootRef.child("Users").child(CurrentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.hasChild("name") && dataSnapshot.hasChild("image")) {
+
+                    String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                    String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+
+                    String retrieveprofileImage = dataSnapshot.child("image").getValue().toString();
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveStatus);
+
+                } else if (dataSnapshot.exists() && dataSnapshot.hasChild("name")) {
+
+                    String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                    String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveStatus);
+
+                } else {
+                    userName.setVisibility(View.VISIBLE);
+                    Toast.makeText(SettingsActivity.this, "Pleas set & Update Your Profile", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
@@ -58,7 +97,7 @@ public class SettingsActivity extends AppCompatActivity {
             Toast.makeText(this, "Please Enter UserName", Toast.LENGTH_SHORT).show();
 
         } else if (TextUtils.isEmpty(setStatus)) {
-            Toast.makeText(this, "Please Enter Your Statuse", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please Enter Your Status", Toast.LENGTH_SHORT).show();
 
 
         } else {
