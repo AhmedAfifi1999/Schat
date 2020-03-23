6ca3b3de9e05fd14940640278d1acdde07a87492
AhmedAfifi1999 <ahmedafifi1500@gmail.com>
2020-03-23 23:17:11 +0200

    profile Activity display Image username , userStatus in activity

--- app/src/main/java/com/example/schat/ProfileActivity.java
+++ app/src/main/java/com/example/schat/ProfileActivity.java
@@ -1,11 +1,19 @@
 package com.example.schat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -16,21 +24,61 @@ public class ProfileActivity extends AppCompatActivity {
     private Button SendMessageRequestBtn;
     private String reciverUserID;
 
+    private DatabaseReference UserRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-/*
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
         reciverUserID = getIntent().getExtras().get("visit_user_id").toString();
-        Toast.makeText(this, "User ID: " + reciverUserID, Toast.LENGTH_SHORT).show();
-        */
 
         userProfileName = findViewById(R.id.visit_user_name);
         userProfileStatus = findViewById(R.id.visit_profile_status);
         SendMessageRequestBtn = findViewById(R.id.send_message_request_button);
         userProfileImage = findViewById(R.id.visit_profile_image);
 
+        RetrieveUserInfo();
+
 
     }
+
+    private void RetrieveUserInfo() {
+
+        UserRef.child(reciverUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if ((dataSnapshot.exists() && dataSnapshot.hasChild("image"))) {
+
+                    String image = dataSnapshot.child("image").getValue().toString();
+                    String userName = dataSnapshot.child("name").getValue().toString();
+                    String userStatus = dataSnapshot.child("status").getValue().toString();
+                    // First  solve Link Image In Firebase Then Delete this commint
+                    Picasso.get().load(image).placeholder(R.drawable.profile_image).into(userProfileImage);
+                    userProfileName.setText(userName);
+                    userProfileStatus.setText(userStatus);
+                } else {
+                    String userName = dataSnapshot.child("name").getValue().toString();
+                    String userStatus = dataSnapshot.child("status").getValue().toString();
+
+                    userProfileName.setText(userName);
+                    userProfileStatus.setText(userStatus);
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
