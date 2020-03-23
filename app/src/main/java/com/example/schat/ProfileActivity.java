7460c704ace27e600fae953e5ad63b3923e5037e
AhmedAfifi1999 <ahmedafifi1500@gmail.com>
2020-03-23 14:19:51 +0200

    profile Activity  add find id > in profileActivity.class

--- app/src/main/java/com/example/schat/ProfileActivity.java
+++ app/src/main/java/com/example/schat/ProfileActivity.java
@@ -3,11 +3,17 @@ package com.example.schat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-
+    private TextView userProfileName, userProfileStatus;
+    private CircleImageView userProfileImage;
+    private Button SendMessageRequestBtn;
     private String reciverUserID;
 
     @Override
@@ -15,8 +21,16 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+/*
         reciverUserID = getIntent().getExtras().get("visit_user_id").toString();
         Toast.makeText(this, "User ID: " + reciverUserID, Toast.LENGTH_SHORT).show();
+        */
+
+        userProfileName = findViewById(R.id.visit_user_name);
+        userProfileStatus = findViewById(R.id.visit_profile_status);
+        SendMessageRequestBtn = findViewById(R.id.send_message_request_button);
+        userProfileImage = findViewById(R.id.visit_profile_image);
+
 
     }
 }
