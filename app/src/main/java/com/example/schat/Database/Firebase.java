7896db699c1c843aebb2418a491585fb9dd16d7a
AhmedAfifi1999 <ahmedafifi1500@gmail.com>
2020-03-24 21:51:57 +0200

    profile Activity add some Conf

--- app/src/main/java/com/example/schat/Database/Firebase.java
+++ app/src/main/java/com/example/schat/Database/Firebase.java
@@ -1,4 +1,8 @@
 package com.example.schat.Database;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Firebase {
+
+
 }
--- app/src/main/java/com/example/schat/View/Activites/ProfileActivity.java
+++ app/src/main/java/com/example/schat/View/Activites/ProfileActivity.java
@@ -1,24 +1,69 @@
 package com.example.schat.View.Activites;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.schat.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private CircleImageView userProfileImage;
+    private TextView userProfileName, userProfileStatue;
+    private Button SendMessageRequestBtn;
+    private String reciverUserID, SenderUserID;
 
-    private String reciverUserID;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UserRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
+        SenderUserID = mAuth.getCurrentUser().getUid();
+
         reciverUserID = getIntent().getExtras().get("visit_user_id").toString();
-        Toast.makeText(this, "User ID: " + reciverUserID, Toast.LENGTH_SHORT).show();
+
+
+        userProfileImage = findViewById(R.id.visit_profile_image);
+        userProfileName = findViewById(R.id.visit_user_name);
+        userProfileStatue = findViewById(R.id.visit_profile_status);
+
+        RetrieveUserInfo();
+    }
+
+    private void RetrieveUserInfo() {
+        UserRef.child(reciverUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.hasChild("image")) {
+    
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
